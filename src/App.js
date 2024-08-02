830329033d5de2832a2fbea3408aa533ebe1b55e
Kevin Nelson <km.nelson@outlook.com>
2024-08-01 21:23:44 -0400

    Added Card to file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState, useEffect } from "react"
 import { getAppointments, postAppointment } from './api/apiCalls';
 import './App.css';
+import Form from './Components/Form/Form'
+import Appointments from './Components/Appointments/Appointments'
 
 function App() {
   return (
     <div className="App">
      <h1>Appointments!</h1>
+     <Form />
+     <Appointments />
     </div>
   );
 }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+
+const Card = ({ pet, date, id, time, deleteAppointment }) => {
+    return (
+        <div className='appointment-card'>
+            <h3>{pet}</h3>
+            <p>{date}</p>
+            <p>{time}</p>
+            <button onClick={() => deleteAppointment(id)}>🗑️</button>
+        </div>
+    )
+}
