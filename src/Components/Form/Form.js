8d744c418870ba4e3083059eb23c0116fe7ff675
Kevin Nelson <km.nelson@outlook.com>
2024-08-01 22:02:33 -0400

    Added controlled form to match the data strucuture of API

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import { useState } from 'react'
 
-function Form() {
-  return (
-    <div>Form</div>
-  )
+function Form({addAppointment}) {
+    const [pet, setPet] = useState("")
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
+
+    function submitAppointment(event) {
+        event.preventDefault()
+        const newAppointment = {
+            id: Date.now(),
+            pet,
+            date,
+            time
+        }
+        addAppointment(newAppointment)
+        clearInput()
+    }
+
+    function clearInput() {
+        setPet("")
+        setDate("")
+        setTime("")
+    }
+
+    return (
+        <form>
+            <input
+                type='text'
+                placeholder='Pet Name'
+                name='pet'
+                value={pet}
+                onChange={event => setPet(event.target.value)}
+            />
+            <input
+                type='text'
+                placeholder='Date'
+                name='date'
+                value={date}
+                onChange={event => setDate(event.target.value)}
+            />
+            <input
+                type='text'
+                placeholder='Time'
+                name='time'
+                value={time}
+                onChange={event => setTime(event.target.value)}
+            />
+            <button onClick={submitAppointment}>SUBMIT</button>
+        </form>
+    )
 }
 
 export default Form
